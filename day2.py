9cb6ff10e2b724ba3e6f11b9d2e2980d789f5015
Shivansh-Rai <shivanshrai2316@gmail.com>
2026-03-08 21:54:06 +0530

    day 2 task update

---
+++ day2.py
@@ -0,0 +1,25 @@
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+
+
+def vectorize(a, b):
+    v = TfidfVectorizer()
+    x = v.fit_transform([a, b])
+    return x
+
+
+def score(x):
+    s = cosine_similarity(x[0:1], x[1:2])
+    return s[0][0]
+
+
+print("Enter first document:")
+a=input()
+
+print("Enter second document:")
+b=input()
+
+x=vectorize(a,b)
+s=score(x)
+
+print("Cosine Similarity:", s)
