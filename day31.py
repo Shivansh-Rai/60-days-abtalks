dce1aeba3d9b3832a60252d30d299178f55b5c31
Shivansh-Rai <shivanshrai2316@gmail.com>
2026-04-04 20:56:40 +0530

    removed api key

---
+++ day31.py
@@ -0,0 +1,18 @@
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_openai import ChatOpenAI
+
+
+llm = ChatOpenAI(api_key="YOUR_API_KEY", model="gpt-4o-mini")
+
+prompt = ChatPromptTemplate.from_template("Explain the following in simple terms:\n{input}")
+
+chain = prompt | llm
+
+
+print("Enter text:")
+t = input()
+
+res = chain.invoke({"input": t})
+
+print("\nResponse:")
+print(res.content)
