d5b0d684f2a5617b035d2fbe39094413fef15cf0
Shivansh-Rai <shivanshrai2316@gmail.com>
2026-04-10 21:34:02 +0530

    updated

---
+++ day36.py
@@ -0,0 +1,24 @@
+from langchain_openai import ChatOpenAI
+
+from langchain_core.messages import HumanMessage, AIMessage
+
+
+llm = ChatOpenAI(api_key="YOUR_API_KEY", model="gpt-4o-mini")
+
+history = []
+
+print("Chatbot started (type 'exit' to stop)")
+
+while True:
+    q = input("You: ")
+    
+    if q.lower() == "exit":
+        break
+
+    history.append(HumanMessage(content=q))
+
+    r = llm.invoke(history)
+
+    history.append(AIMessage(content=r.content))
+
+    print("Bot:", r.content)
