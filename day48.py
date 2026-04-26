86a7d1b4a26819114f74a5447d52317e945df2fd
Shivansh-Rai <shivanshrai2316@gmail.com>
2026-04-26 16:58:14 +0530

    done

---
+++ day48.py
@@ -0,0 +1,44 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import LogisticRegression
+
+
+app = FastAPI()
+
+
+class Input(BaseModel):
+    text: str
+
+
+def train():
+    docs = [
+        "I love this product",
+        "This is terrible",
+        "Amazing experience",
+        "Worst service ever"
+    ]
+    y = [1, 0, 1, 0]
+
+    v = TfidfVectorizer()
+    x = v.fit_transform(docs)
+
+    m = LogisticRegression()
+    m.fit(x, y)
+
+    return v, m
+
+
+vectorizer, model = train()
+
+
+@app.get("/")
+async def home():
+    return {"message": "ASGI FastAPI App Running"}
+
+
+@app.post("/predict")
+async def predict(data: Input):
+    x = vectorizer.transform([data.text])
+    y = model.predict(x)[0]
+    return {"prediction": int(y)}
